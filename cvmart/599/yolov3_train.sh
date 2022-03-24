23c037d8b1b5fcb4854891af8c93f0f02342dc83
vansin <msnode@163.com>
2022-03-24 16:52:23 +0800

    feat: yolo 599

--- cvmart/599/yolov3_train.sh
+++ cvmart/599/yolov3_train.sh
@@ -1,3 +1,3 @@
 python /project/mmlab_template/tools/dataset_converters/cvmarr_599_to_coco.py
 # python /project/train/mmlab_template/tools/train.py /project/train/mmlab_template/configs/icdar2019_fire/faster_rcnn/faster_rcnn_r101_fpn_1x_coco.py --work-dir /project/train/models
-python /project/mmlab_template/tools/train.py /project/mmlab_template/configs/599/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py --work-dir /project/train/models
+python /project/mmlab_template/tools/train.py /project/mmlab_template/configs/599/yolo/yolov3_mobilenetv2_320_300e_coco.py --work-dir /project/train/models/yolov3_mobilenetv2_320_300e_coco --auto-resume

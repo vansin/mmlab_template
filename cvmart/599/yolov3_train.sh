3c9d36a43abae2d747510f9e9f4a563de8da63d4
vansin <msnode@163.com>
2022-03-24 16:58:15 +0800

    feat: enhance the shell

--- cvmart/599/yolov3_train.sh
+++ cvmart/599/yolov3_train.sh
@@ -1,3 +1,5 @@
-python /project/mmlab_template/tools/dataset_converters/cvmarr_599_to_coco.py
+export DNAME=599
+
+python /project/mmlab_template/tools/dataset_converters/cvmarr_${DNAME}_to_coco.py
 # python /project/train/mmlab_template/tools/train.py /project/train/mmlab_template/configs/icdar2019_fire/faster_rcnn/faster_rcnn_r101_fpn_1x_coco.py --work-dir /project/train/models
-python /project/mmlab_template/tools/train.py /project/mmlab_template/configs/599/yolo/yolov3_mobilenetv2_320_300e_coco.py --work-dir /project/train/models/yolov3_mobilenetv2_320_300e_coco --auto-resume
+python /project/mmlab_template/tools/train.py /project/mmlab_template/configs/${DNAME}/yolo/yolov3_mobilenetv2_320_300e_coco.py --work-dir /project/train/models/${DNAME} --auto-resume

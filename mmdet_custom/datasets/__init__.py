afc056588d0a0143a8c683b9d57e6697c85fd548
vansin <vansin@aliyun.com>
2022-03-25 16:40:12 +0800

    feat: update dateset 10100

--- mmdet_custom/datasets/__init__.py
+++ mmdet_custom/datasets/__init__.py
@@ -2,4 +2,6 @@ from .tabnet import TableDataset
 from .fire import FireDataset
 from .d816 import D816Dataset
 from .d877 import D877Dataset
-__all__ = ['TableDataset', 'FireDataset', 'D816Dataset', 'D877Dataset']
+from .d10100 import D10100Dataset
+__all__ = ['TableDataset', 'FireDataset',
+           'D816Dataset', 'D877Dataset', 'D10100Dataset']

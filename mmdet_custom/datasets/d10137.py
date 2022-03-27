9a7fee80aa7b95a4cc2fce6b603d843215e744ab
vansin <vansin@aliyun.com>
2022-03-27 10:52:14 +0800

    feat: add the ,

--- mmdet_custom/datasets/d10137.py
+++ mmdet_custom/datasets/d10137.py
@@ -23,7 +23,7 @@ from mmdet.core import eval_map, eval_recalls
 @DATASETS.register_module()
 class D10137Dataset(CustomDataset):
 
-    CLASSES = ('person')
+    CLASSES = ('person', )
 
     PALETTE = [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230),
                (106, 0, 228), (0, 60, 100), (0, 80, 100), (0, 0, 70),

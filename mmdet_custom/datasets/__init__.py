9d498ea7f04f155816e482f017ccc585721f43a3
vansin <vansin@aliyun.com>
2022-03-27 10:36:26 +0800

    feat: update d10137

--- mmdet_custom/datasets/__init__.py
+++ mmdet_custom/datasets/__init__.py
@@ -3,5 +3,6 @@ from .fire import FireDataset
 from .d816 import D816Dataset
 from .d877 import D877Dataset
 from .d10100 import D10100Dataset
+from .d10137 import D10137Dataset
 __all__ = ['TableDataset', 'FireDataset',
-           'D816Dataset', 'D877Dataset', 'D10100Dataset']
+           'D816Dataset', 'D877Dataset', 'D10100Dataset', 'D10137Dataset']

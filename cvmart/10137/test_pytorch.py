39167c3742e8f5e54a9749129048bd3e47153b29
vansin <msnode@163.com>
2022-03-31 17:15:53 +0800

    feat: update

--- cvmart/10137/test_pytorch.py
+++ cvmart/10137/test_pytorch.py
@@ -7,7 +7,7 @@ from mmdet_custom.datasets import D10137Dataset
 def init():
 
     config_file = '/project/train/models/10137/yolov3_mobilenetv2_320_300e_coco.py'
-    checkpoint_file = '/project/train/models/10137/epoch_5.pth'
+    checkpoint_file = '/project/train/models/10137/latest.pth'
     # build the model from a config file and a checkpoint file
     model = init_detector(config_file, checkpoint_file)
     return model

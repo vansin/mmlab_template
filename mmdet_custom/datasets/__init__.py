e41fb3d74f81c82ad26c4404d63a4476f14d6a9e
vansin <msnode@163.com>
2022-03-31 13:11:51 +0800

    fix: dataset

--- mmdet_custom/datasets/__init__.py
+++ mmdet_custom/datasets/__init__.py
@@ -4,6 +4,6 @@ from .d816 import D816Dataset
 from .d877 import D877Dataset
 from .d10100 import D10100Dataset
 from .d10137 import D10137Dataset
-from .d10032 import D10137Dataset
+from .d10032 import D10032Dataset
 __all__ = ['TableDataset', 'FireDataset',
-           'D816Dataset', 'D877Dataset', 'D10100Dataset', 'D10137Dataset', 'D10137Dataset']
+           'D816Dataset', 'D877Dataset', 'D10100Dataset', 'D10137Dataset', 'D10032Dataset']

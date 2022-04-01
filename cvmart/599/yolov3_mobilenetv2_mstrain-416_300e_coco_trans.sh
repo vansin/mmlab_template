24f5d8decc9e86bcc8869b36c40e5bd9a1ac3cd9
vansin <vansin@aliyun.com>
2022-04-01 10:34:29 +0800

    feat: update

---
+++ cvmart/599/yolov3_mobilenetv2_mstrain-416_300e_coco_trans.sh
@@ -0,0 +1,24 @@
+export DNAME=599
+
+# python /project/mmlab_template/tools/dataset_converters/cvmarr_${DNAME}_to_coco.py
+# # python /project/train/mmlab_template/tools/train.py /project/train/mmlab_template/configs/icdar2019_fire/faster_rcnn/faster_rcnn_r101_fpn_1x_coco.py --work-dir /project/train/models
+# python /project/mmlab_template/tools/train.py /project/mmlab_template/configs/${DNAME}/yolo/yolov3_mobilenetv2_mstrain-416_300e_coco.py --work-dir /project/train/models/${DNAME} --auto-resume
+# #bash /project/mmlab_template/cvmart/599/yolov3_mobilenetv2_mstrain-416_300e_coco.sh
+
+
+export TENSORRT_DIR=/project/TensorRT-8.2.3.0
+export LD_LIBRARY_PATH=$TENSORRT_DIR/lib:$TENSORRT_DIR
+export CUDNN_DIR=/project/cuda
+export LD_LIBRARY_PATH=${CUDNN_DIR}/lib:${LD_LIBRARY_PATH}
+
+python /project/mmlab_template/mmdeploy/tools/deploy.py \
+/project/mmlab_template/mmdeploy/configs/mmdet/detection/detection_tensorrt_dynamic-320x320-1344x1344.py \
+/project/mmlab_template/configs/${DNAME}/yolo/yolov3_mobilenetv2_mstrain-416_300e_coco.py \
+/project/train/models/${DNAME}/epoch_10.pth \
+/home/data/599/fire_p_190.jpg \
+--work-dir /project/train/models/${DNAME} \
+--device cuda \
+--dump-info
+
+
+

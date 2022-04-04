586af596c5afc10c5314d47b5dc40854592b6d0c
vansin <msnode@163.com>
2022-04-04 13:48:38 +0800

    fea: update test_trt

--- cvmart/599/test_trt.py
+++ cvmart/599/test_trt.py
@@ -1,3 +1,50 @@
+# import json
+# from mmdet.apis import init_detector, inference_detector
+# import mmcv
+
+# def init():
+
+#     config_file = '/project/train/models/599/yolov3_mobilenetv2_mstrain-416_300e_coco.py'
+#     checkpoint_file = '/project/train/models/599/latest.pth'
+#     # build the model from a config file and a checkpoint file
+#     model = init_detector(config_file, checkpoint_file)
+#     return model
+
+# def process_image(handle=None, input_image=None, args=None, **kwargs):
+    
+#     result = inference_detector(handle, input_image)
+#     # Process image here
+#     objects = []
+#     fires = result[0]
+#     for fire in fires:
+#         obj = dict(
+#             xmin = int(fire[0].item()),
+#             ymin= int(fire[1].item()),
+#             xmax=int(fire[2].item()),
+#             ymax=int(fire[3].item()),
+#             confidence=fire[4].item(),
+#             name = "fire"
+#         )
+
+#         if obj['confidence']>0.5:
+#             objects.append(obj)
+
+#     # model.show_result(img, result)
+#     # model.show_result(img, result, out_file='result.jpg', score_thr = 0.3)
+
+#     r_json = dict()
+#     r_json['algorithm_data'] = dict(target_info=objects, is_alert=False, target_count=0)
+#     r_json['model_data'] = dict(objects=objects)
+
+#     if objects.__len__()>0:
+#         r_json['algorithm_data']['is_alert'] = True
+#         r_json['algorithm_data']['target_count'] = objects.__len__()
+
+#     # return json.dumps(objects, indent=4)
+
+#     return json.dumps(r_json, indent=4)
+
+
 import json
 from mmdet_custom.datasets import FireDataset as NormalDataset
 import os
@@ -6,14 +53,20 @@ os.system('export LD_LIBRARY_PATH=$TENSORRT_DIR/lib:$TENSORRT_DIR')
 os.system('export CUDNN_DIR=/project/cuda')
 os.system('export LD_LIBRARY_PATH=${CUDNN_DIR}/lib64:${LD_LIBRARY_PATH}')
 
+
+
+
 import sys
 sys.path.append('/project/mmlab_template/mmdeploy/build/lib/')
 import mmdeploy_python
 
+os.system('echo $LD_LIBRARY_PATH')
+
+
 
 def init():
     
-    detector = mmdeploy_python.Detector('/project/train/599/models', 'cuda', 0)
+    detector = mmdeploy_python.Detector('/project/train/models/599', 'cuda', 0)
     return detector
 
 def process_image(handle=None, input_image=None, args=None, **kwargs):
@@ -30,10 +83,10 @@ def process_image(handle=None, input_image=None, args=None, **kwargs):
 
         print(i,j,k)
         obj = dict(
-            xmin=i[0].item(),
-            ymin=i[1].item(),
-            xmax=i[2].item(),
-            ymax=i[3].item(),
+            xmin=int(i[0].item()),
+            ymin=int(i[1].item()),
+            xmax=int(i[2].item()),
+            ymax=int(i[3].item()),
             confidence=i[4].item(),
             name=NormalDataset.CLASSES[j.item()]
         )

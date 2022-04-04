defd6a9313f3292f1814f44a74cd47e67276941a
vansin <msnode@163.com>
2022-04-04 16:10:01 +0800

    feat: update 10032

--- cvmart/10032/fasterrcnn_train.sh
+++ cvmart/10032/fasterrcnn_train.sh
@@ -1,3 +1,4 @@
-python /project/mmlab_template/tools/dataset_converters/cvmarr_599_to_coco.py
+export DNAME=10032
+python /project/mmlab_template/tools/dataset_converters/cvmarr_${DNAME}_to_coco.py
 # python /project/train/mmlab_template/tools/train.py /project/train/mmlab_template/configs/icdar2019_fire/faster_rcnn/faster_rcnn_r101_fpn_1x_coco.py --work-dir /project/train/models
-python /project/mmlab_template/tools/train.py /project/mmlab_template/configs/599/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py --work-dir /project/train/models
+python /project/mmlab_template/tools/train.py /project/mmlab_template/configs/599/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py --work-dir /project/train/models/${DNAME} --auto-resume

0a85de9403801b52d17579baf0af741e7df3d3b5
vansin <msnode@163.com>
2022-04-09 12:18:01 +0800

    feat: update

---
+++ cvmart/10185/yolox/yolox_x_8x8_300e_coco/train.sh
@@ -0,0 +1,10 @@
+export DNAME=10185
+export algorthm_path=yolox/yolox_x_8x8_300e_coco
+
+cd /project/mmlab_template
+python /project/mmlab_template/tools/dataset_converters/cvmarr_${DNAME}_to_coco.py
+# python /project/train/mmlab_template/tools/train.py /project/train/mmlab_template/configs/icdar2019_fire/faster_rcnn/faster_rcnn_r101_fpn_1x_coco.py --work-dir /project/train/models
+python /project/mmlab_template/tools/train_new.py /project/mmlab_template/configs/${DNAME}/${algorthm_path}.py --work-dir /project/train/models --auto-resume
+
+
+#bash /project/mmlab_template/cvmart/599/yolov3_mobilenetv2_mstrain-416_300e_coco.sh

c7dd1a4e8d98a6fd0c42e50149b5922802d19e09
vansin <msnode@163.com>
2022-05-08 15:30:21 +0800

    feat: add notify function

--- common/notify/notify_robot.py
+++ common/notify/notify_robot.py
@@ -3,13 +3,15 @@ import os
 
 # "2aac663c6d2f"
 def NotifyRobot(title, name, content):
-    notify_token = os.getenv('notify_token') 
+    AUTODL_NOTIFY_TOKEN = os.getenv('AUTODL_NOTIFY_TOKEN')
+    print(AUTODL_NOTIFY_TOKEN)
     resp = requests.post("https://www.autodl.com/api/v1/wechat/message/push",
                         json={
-                            "token": notify_token,
+                            "token": AUTODL_NOTIFY_TOKEN,
                             "title": title,
                             "name": name,
                             "content": content
                         })
+
     print(resp.content.decode())
 
